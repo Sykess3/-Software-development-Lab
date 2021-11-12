f3693663e6a635c480a2bc4faa1d99c3805a900e
Nikolay Bulyha <thesykess@gmail.com>
2021-11-12 20:09:37 +0200

    Added Contracts of Repositories

---
+++ DAL/Repositories/Interfaces/IAuthorizationInfoRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace DAL.Repositories.Interfaces
+{
+    public interface IAuthorizationInfoRepository
+    {
+        
+    }
+}
---
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace DAL.Repositories.Interfaces
+{
+    public class IRepository
+    {
+        
+    }
+}
